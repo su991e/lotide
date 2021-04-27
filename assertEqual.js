f3c0a0fa495112d5828b36e569ed70172afedd30
Youngseok Bae <young.seok@hotmail.com>
2021-04-27 00:14:41 +0000

    commiting assert exercise

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("😇😇 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😠😠 Assertion Failed: " + actual + " !== " + expected);
+  }
+
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
+assertEqual("Youngseok", "Youngsek");
