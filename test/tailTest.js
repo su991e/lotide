5c0dda3a889388ce427e3bd12060d96ee161ea69
Youngseok Bae <young.seok@hotmail.com>
2021-05-03 19:42:23 +0000

    Refactored tailTest using Mocah&Chai assertion

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,23 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
+//const tail = require('../tail');
+//
+//const result = tail(["Hello", "Lighthouse", "Labs"]);
+//assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+//
+//// Test Case: Check the original array
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+////console.log(words);
+////console.log(tail(words));
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-//console.log(words);
-//console.log(tail(words));
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [7, 9] for [1, 7, 9]", () => {
+    assert.deepEqual(tail([1, 7, 9]), [7, 9]);
+  });
+});
